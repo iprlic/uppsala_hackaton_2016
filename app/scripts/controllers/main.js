f8f61f2ace3cfe80051da0bc3fc3093b67b3cb31
Ivan Prlic <ip@snowfall.se>
2016-10-22 13:04:22 +0200

    initial commit, bolier plate

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uppsalaHackaton2016App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uppsalaHackaton2016App
+ */
+angular.module('uppsalaHackaton2016App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
