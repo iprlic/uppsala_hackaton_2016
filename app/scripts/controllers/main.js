97cc6a4420918687426bf08678351dda04e4df2a
Johanna Nohldén <johannanohlden@Johanna-Nohldens-MacBook.local>
2016-10-22 15:17:07 +0200

    more fixes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,12 @@
  * Controller of the uppsalaHackaton2016App
  */
 angular.module('uppsalaHackaton2016App')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, uiGmapGoogleMapApi) {
     $scope.drones = [
     	{
     		'id': 'Drone1',
-    		'posX': 200,
-    		'posY': 100,
+    		'posX': 66.3673995,
+    		'posY': 20.9953064,
     		'areaX': 0,
     		'areaY': 0,
     		'width': 300,
@@ -21,8 +21,8 @@ angular.module('uppsalaHackaton2016App')
     	},
     	{
     		'id': 'Drone2',
-    		'posX': 487,
-    		'posY': 100,
+        'posX': 65.3673995,
+        'posY': 20.9911111,
     		'areaX': 200,
     		'areaY': 0,
     		'width': 300,
@@ -33,9 +33,16 @@ angular.module('uppsalaHackaton2016App')
     $scope.showDrone = function(drone) {
     	// show drone on map
     	console.log(drone);
+
+      $scope.map.center.latitude = drone.posX;
+      $scope.map.center.longitude = drone.posY;
+
+
     }
 
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    $scope.map = {
+      center:
+      { latitude: 45, longitude: -73 }, zoom: 8 };
 
     // uiGmapGoogleMapApi is a promise.
     // The "then" callback function provides the google.maps object.
