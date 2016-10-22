f1abcca52b88db4129741af67ad56ccbf2c3a83c
Martin Bjorling <Martin Bjorling>
2016-10-22 14:14:38 +0200

    First layout stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,30 @@
  * Controller of the uppsalaHackaton2016App
  */
 angular.module('uppsalaHackaton2016App')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  .controller('MainCtrl', function ($scope) {
+    $scope.drones = [
+    	{
+    		'id': 'Drone1',
+    		'posX': 200,
+    		'posY': 100,
+    		'areaX': 0,
+    		'areaY': 0,
+    		'width': 300,
+    		'height': 300
+    	},
+    	{
+    		'id': 'Drone2',
+    		'posX': 487,
+    		'posY': 100,
+    		'areaX': 200,
+    		'areaY': 0,
+    		'width': 300,
+    		'height': 300
+    	}
     ];
+
+    $scope.showDrone = function(drone) {
+    	// show drone on map
+    	console.log(drone);
+    }
   });
